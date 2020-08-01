fb80d8b17df16a4b8582aad0b0c1c60775ae221f
Tieg <tiegosullivanpsnl@gmail.com>
2020-08-01 11:08:32 +0200

    super healthy master code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -18,6 +19,7 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        print('works')
         # logic to determine if user exists
 
         # replace if true with logic to determine if user is a business
