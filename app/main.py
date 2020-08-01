9b910104afa20962512a174541fe2c40272dab4a
Tieg <tiegosullivanpsnl@gmail.com>
2020-08-01 10:24:09 +0200

    :sparkles: some routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.form.getlist('isbusiness') == []:
+        # takes employee to employee profile
+        return render_template('employee.html')
+    else:
+        # takes you to the business profile
+        return render_template('business.html')
+
+@app.route('/login', methods=['GET'])
+def getlogin():
+    return render_template('login.html')
+
+@app.route('/login/activate', methods=['POST'])
+def login():
+    # logic to determine if user exists
+
+    # replace if true with logic to determine if user is a business
+    if True:
+        return render_template('business.html')
+    else:
+        return render_template('jobs.html')
