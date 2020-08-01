2177ea531d870310cb3aae3a4141ec2f7375fb1e
Troy Ian Verwayen <21884528@sun.ac.za>
2020-08-01 11:31:08 +0200

    added db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,89 @@
 from flask import Flask, render_template, request, redirect, url_for
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
 
+
+# Class-based application configuration
+class ConfigClass(object):
+    """ Flask application config """
+
+    # Flask settings
+    SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
+
+    # Flask-SQLAlchemy settings
+    SQLALCHEMY_DATABASE_URI = 'postgres://kumngmqflcmynz:4200ddda909a206db818cef5a42c53fc15f38d0a2dfff8dddf546eaf1cc42dfd@ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/draq6o35ulr54'    # File-based SQL database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids SQLAlchemy warning
+
+    # Flask-User settings
+    USER_APP_NAME = "Flask-User Basic App"      # Shown in and email templates and page footers
+    USER_ENABLE_EMAIL = True       # Enable email authentication
+    USER_ENABLE_USERNAME = False    # Disable username authentication
+    USER_EMAIL_SENDER_NAME = USER_APP_NAME
+    USER_EMAIL_SENDER_EMAIL = "noreply@example.com"
 
 app = Flask(__name__)
+app.config.from_object(__name__+'.ConfigClass')
+
+# Initialize Flask-SQLAlchemy
+db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+
+    # User authentication information. The collation='NOCASE' is required
+    # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    email_confirmed_at = db.Column(db.DateTime())
+    password = db.Column(db.String(255), nullable=False, server_default='')
+
+    # User information
+    first_name = db.Column(db.String(100), nullable=False, server_default='')
+    last_name = db.Column(db.String(100), nullable=False, server_default='')
+
+    # Define the relationship to Role via UserRoles
+    roles = db.relationship('Role', secondary='user_roles')
+
+# Define the Role data-model
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+# Define the UserRoles association table
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+# Create all database tables
+db.create_all()
+
+# Create 'member@example.com' user with no roles
+if not User.query.filter(User.email == 'member@example.com').first():
+    user = User(
+        email='member@example.com',
+        email_confirmed_at=datetime.datetime.utcnow(),
+        password=user_manager.hash_password('Password1'),
+    )
+    db.session.add(user)
+    db.session.commit()
+
+# Create 'admin@example.com' user with 'Admin' and 'Agent' roles
+if not User.query.filter(User.email == 'admin@example.com').first():
+    user = User(
+        email='admin@example.com',
+        email_confirmed_at=datetime.datetime.utcnow(),
+        password=user_manager.hash_password('Password1'),
+    )
+    user.roles.append(Role(name='Admin'))
+    user.roles.append(Role(name='Agent'))
+    db.session.add(user)
+    db.session.commit()
+
 
 @app.route('/', methods=['GET'])
 def index():
